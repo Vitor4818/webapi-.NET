dd1665c542e6a9483a45509c0ef70870c68e2272
Vitor4818 <vitorg.martins4818@gmail.com>
2025-05-07 01:10:52 -0300

    feat: integração dos projetos e configuração inicial da API funcionando

---
+++ CandidatosApi/Program.cs
@@ -0,0 +1,24 @@
+using CandidatosBusiness;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<CandidatoService>();
+// Adiciona os serviços para os controladores
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+//app.UseHttpsRedirection();
+
+app.MapControllers();
+app.Run();
+
---
+++ CandidatosModel/CandidatoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CandidatosModel;
+public class CandidatoModel
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Partido { get; set; }
+    public int Idade { get; set; }
+}
